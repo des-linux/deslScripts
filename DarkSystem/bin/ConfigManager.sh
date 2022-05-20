6c528e5a3f4e08c0cc71a3788e6d2b648ce6c9b3
Hikaru Kurosaki <hikaru-oss@xprj.org>
2022-05-21 02:27:05 +0900

    File header whitespace unified to tab

--- DarkSystem/bin/ConfigManager.sh
+++ DarkSystem/bin/ConfigManager.sh
@@ -1,8 +1,8 @@
 #!INCLUDE_ONLY
 #//////////////////////////////////////////////////
 #//DESLinux Builder
-#//     (C)2014-2022 Dark Embedded Systems.
-#//     http://xprj.net/
+#//	(C)2014-2022 Dark Embedded Systems.
+#//	http://xprj.net/
 #//////////////////////////////////////////////////
 
 config_get_env(){
